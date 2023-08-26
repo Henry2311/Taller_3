0fa6f42ee687f2391b0e9a4a79a75b44f3a8a145
Henry <3020673530101@ingenieria.usac.edu.gt>
2023-08-25 22:17:25 -0600

    fix

--- config/credentials.py
+++ config/credentials.py
@@ -3,6 +3,6 @@
 DB_USER = 'root'   #nombre de usuario
 DB_PASSWORD = '1234' #contraseña
 DB_HOST = 'localhost' #host de la bd
-DB_NAME = 'aydt1' #nombre de la DB
+DB_NAME = 'taller3' #nombre de la DB
 
 DB_URL = f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
